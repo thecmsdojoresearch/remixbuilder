18355d8cac18b1a90983c8a447f9bc3e8c537299
jim zike huang <jim.javathunderbird@gmail.com>
2022-06-16 11:13:53 -0700

    enhance view

--- .remixapp/app/webclient/views/index/show.tsx
+++ .remixapp/app/webclient/views/index/show.tsx
@@ -1,15 +1,38 @@
-import IndexStore from '../../stores/index'; //stores are like models in client side
+import IndexStore from '../../stores/index'; //auto generated
+import { useLoaderData } from "@remix-run/react";
+import { useEffect } from 'react';
 
 export default () => {
-  const store = new IndexStore();
-  store._init();
+  const data = useLoaderData();
+  const store = new IndexStore; 
+  store._init(); 
+
+  useEffect(() => {
+    store.fetchCurrentIP();
+  });
 
   return (
-    <>
-      <h1>Current IP: {store.getIp()}</h1>
-      <button onClick={() => store.fetchCurrentIP() }>
-        Fetch Current IP
-      </button>
-    </>
+    <div>
+      <h1>Version 8</h1>
+      <h1>Via Import</h1>
+      <h1>This button has been clicked for {store.getCounter()} times in passing store</h1>
+      <h4>Current IP Address: {store.getIp()} </h4>
+      <button onClick={()=> { store.incrementCounter() }}>Click</button>
+      <h1>{data.a}</h1>
+
+      <div>
+        {(() => {
+          if (store.getSubmittedMessage().length > 0) {
+            return <h4>Submitted Message: {store.getSubmittedMessage()}</h4> 
+          }
+        })()}
+        <label>Add Message</label>
+        <input type = "text" 
+          value = {store.getMessage()} 
+          onChange = {(e) => {store.setMessage(e.target.value) }}
+        />
+        <button onClick={ ()=> {store.submitMessage()} }>Submit Message</button>
+      </div>
+    </div>
   );
 }
