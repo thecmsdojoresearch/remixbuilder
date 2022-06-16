62637a40f48749f327ecc57c66df563949544392
jim zike huang <jim.javathunderbird@gmail.com>
2022-06-15 21:21:21 -0700

    init builder

--- build.js
+++ build.js
@@ -1,4 +1,17 @@
 /**
  * the main file to start the build process
  */
+const Rsync = require('rsync');
 
+const rsync = new Rsync()
+  .set('delete')
+  .set('progress')
+  .flags('az')
+  .source('./libs')
+  .destination('.remixapp/app');
+
+  rsync.execute((error, code, cmd) => {
+    if (error === null) {
+      console.log("Build synchronized");
+    }
+  });
