5386bda0b0c9d551f0971af32dfad4213f07c4bf
Jim Zike Huang <jim.javathunderbird@gmail.com>
2022-06-18 09:52:26 -0700

    new idea for if statements in template

--- parse.js
+++ parse.js
@@ -7,22 +7,32 @@ const html = fs.readFileSync('./app/routes/index/template.client.html').toString
 const tree = parse(html);
 
 tree.forEach((node, index) => {
-  if (node.name == "button") {
-    if (node.attribs['@click'] !== undefined) {
+  if (node.type == "tag") {
+    if (node.attribs['@if'] !== undefined) {
       const expression = node.attribs['@click'];
-      delete tree[index].attribs['@click'];
-      node.attribs[`onClick={() => {${expression}}}`] = null;
+      console.log(node);
     }
-  } else if (node.name == "input") {
-    if (node.attribs['@value'] !== undefined) {
-      const expression = node.attribs['@value'];
-      delete node.attribs['@value'];
-      node.attribs[`value={${expression}}`] = null;
-    }
-    if (node.attribs['@change'] !== undefined) {
-      const expression = node.attribs['@change'];
-      delete node.attribs['@change'];
-      node.attribs[`onChange = {(e) => {${expression}}}`] = null;
+
+    if (node.name == "button") {
+      //@click directive
+      if (node.attribs['@click'] !== undefined) {
+        const expression = node.attribs['@click'];
+        delete tree[index].attribs['@click'];
+        node.attribs[`onClick={() => {${expression}}}`] = null;
+      }
+    } else if (node.name == "input") {
+      //@value directive
+      if (node.attribs['@value'] !== undefined) {
+        const expression = node.attribs['@value'];
+        delete node.attribs['@value'];
+        node.attribs[`value={${expression}}`] = null;
+      }
+      //@change directive
+      if (node.attribs['@change'] !== undefined) {
+        const expression = node.attribs['@change'];
+        delete node.attribs['@change'];
+        node.attribs[`onChange = {(e) => {${expression}}}`] = null;
+      }
     }
   }
 });
---
+++ switchcase.js
@@ -0,0 +1,9 @@
+const number = 1;
+
+if (number > 3) {
+  console.log("number is > 3");
+}
+else if (number > 5) {
+  console.log("number > 5");
+}
+
