b92fcaed9a620abd75b96cc7703641fb9e6ca5cb
Jim Zike Huang <jim.javathunderbird@gmail.com>
2022-06-18 23:50:40 -0700

    use for of loop

--- .remixapp/app/routes/simple.tsx
+++ .remixapp/app/routes/simple.tsx
@@ -2,18 +2,13 @@ import { If, ElIf, Else } from 'rc-if-else';
 import { For } from 'react-loops';
 
 export default () => {
-  const items = {
-    n1: 1,
-    n2: 2,
-    n3: 3,
-    n4: 4,
-    n5: 5,
-    n6: 6,
-  };
+  const numbers = [
+    1,2,3,4,5,20,376,23,39
+  ];
   return (
     <>
       <ul>
-        <For in={items} as={(number, { key }) =>
+        <For of={numbers} as={(number) =>
           <If condition={number % 2 == 1} >
             <li>{number}</li>
           </If>
