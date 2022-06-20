cae145b47f9bd2c052bd7401d54017274e7cf571
Jim Zike Huang <jim.javathunderbird@gmail.com>
2022-06-19 17:14:59 -0700

    format switch case

--- .remixapp/app/routes/simple_switch_case.tsx
+++ .remixapp/app/routes/simple_switch_case.tsx
@@ -3,9 +3,9 @@ export default () => {
   return (
     <>
       {(() => {switch(true){ //switch
-        case (number > 3): return ( <>
+        case(number > 3): return ( <>
             <h1>number greater than 5</h1>
-            <h1>number greater than 5</h1>
+            <h1>number greater than 5...</h1>
             </>); //endcase
       }})()}
     </>
