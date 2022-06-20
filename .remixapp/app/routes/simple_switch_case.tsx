fd53df9de55222765e836421cffd933d7312a7b4
Jim Zike Huang <jim.javathunderbird@gmail.com>
2022-06-19 17:48:51 -0700

    super flexible switch case

--- .remixapp/app/routes/simple_switch_case.tsx
+++ .remixapp/app/routes/simple_switch_case.tsx
@@ -1,10 +1,12 @@
 export default () => {
   const number = 2;
+
   return (
     <div>
       {(() => {switch(true){
         case(number > 1) : return ( <>
           <h1>number is greater than 1</h1>  
+          <h1>congrat!</h1>  
         </>);
         case(number > 3) : return ( <>
           <h1>number is greater than 3</h1>  
