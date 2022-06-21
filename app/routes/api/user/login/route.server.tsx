98475388c2b4ab51f0b06661d1b4d57703d50eee
Jim Zike Huang <jim.javathunderbird@gmail.com>
2022-06-21 07:20:57 -0700

    prepare for user class

--- app/routes/api/user/login/route.server.tsx
+++ app/routes/api/user/login/route.server.tsx
@@ -3,28 +3,38 @@ const fs = require('fs');
 
 import DB from '~/core/fsdb';
 
-async function authenticate(request){
-  let result = {};
-  const requestPayload = await request.json();
-  const incomingUsername = requestPayload.username.trim();
-  const incomingPassword = requestPayload.password.trim();
-
-  const db = new DB();
-  const cookieSha1 = sha1(request.headers.get('cookie'));
-  const sessionInfo = await db.get(`session/${cookieSha1}`);
-  
-  const userData = await db.get(`user/${incomingUsername}`);
-
-  if (userData.username && userData.passhash && userData.passhash === sha1(incomingPassword)) {
-    result.token = sessionInfo.token;
+class User 
+{
+  async login() {
   }
-  
-  return result;
+
+  async logout() {
+  }
+
+  async authenticate(request){
+    let result = {};
+    const requestPayload = await request.json();
+    const incomingUsername = requestPayload.username.trim();
+    const incomingPassword = requestPayload.password.trim();
+
+    const db = new DB();
+    const cookieSha1 = sha1(request.headers.get('cookie'));
+    const sessionInfo = await db.get(`session/${cookieSha1}`);
+
+    const userData = await db.get(`user/${incomingUsername}`);
+
+    if (userData.username && userData.passhash && userData.passhash === sha1(incomingPassword)) {
+      result.token = sessionInfo.token;
+    }
+
+    return result;
+  }
 }
 
 const route = {
   async action({ request, params }) {
-    const result = await authenticate(request);
+    const user = new User();
+    const result = await user.authenticate(request);
     return result;
   }
 }
