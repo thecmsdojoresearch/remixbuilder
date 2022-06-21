edd6c3c87e5ecb3aea84e7e8909161e735ee2726
Jim Zike Huang <jim.javathunderbird@gmail.com>
2022-06-21 09:36:30 -0700

    check cookie

--- app/routes/index/route.server.tsx
+++ app/routes/index/route.server.tsx
@@ -4,6 +4,12 @@ const sha1 = require('sha1');
 const route = {
   async loader({ request, params }) {
     const data = {};
+    const cookie = request.headers.get('cookie');
+    if (cookie === null) {
+      return {
+      };
+    }
+
     const cookieSha1 = sha1(request.headers.get('cookie'));
     const content = await fs.promises.readFile(`${process.cwd()}/../db/session/${cookieSha1}.json`);
     const authentication = JSON.parse(content.toString());
