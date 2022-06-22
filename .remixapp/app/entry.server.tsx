5428380a74ebd9e1a00c62d6f2ee8d1e58f988c9
Jim Zike Huang <jim.javathunderbird@gmail.com>
2022-06-21 21:00:33 -0700

    custom hack, route api and normal pages

--- .remixapp/app/entry.server.tsx
+++ .remixapp/app/entry.server.tsx
@@ -1,21 +1,46 @@
 import type { EntryContext } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
 import { renderToString } from "react-dom/server";
+import { useEffect } from 'react';
 
 export default function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext
-) {
-  let markup = renderToString(
-    <RemixServer context={remixContext} url={request.url} />
-  );
+) { 
+    if (!request.url.includes('/api')) {
+        responseHeaders.set("Content-Type", "text/html");
+        
+        const comp = () => {
+            return (
+                <>
+                    <head>
+                        <title>Super Homepage</title>
+                    </head>
+                    <body>
+                        <div id="root">
+                            <h1>here</h1>
+                        </div>
+                    </body>
+                </>
+            );
+        };
 
-  responseHeaders.set("Content-Type", "text/html");
+        return new Response("<!DOCTYPE html>" + renderToString(comp()), {
+            status: responseStatusCode,
+            headers: responseHeaders,
+        });
+    } else {
+        responseHeaders.set("Content-Type", "application/json");
 
-  return new Response("<!DOCTYPE html>" + markup, {
-    status: responseStatusCode,
-    headers: responseHeaders,
-  });
+        const markup = renderToString(
+            <RemixServer context={remixContext} url={request.url} />
+        );
+
+        return new Response("<!DOCTYPE html>" + markup, {
+            status: responseStatusCode,
+            headers: responseHeaders,
+        });
+    }
 }
---
+++ .remixapp/app/routes/api/dummy.tsx
@@ -0,0 +1,6 @@
+export const loader = async ({request, response}) => {
+    return {
+        a: 1,
+        b: 2
+    }
+}
