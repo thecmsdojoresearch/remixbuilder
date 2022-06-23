e0ebca8e5406cb5b298a891ab7f13aec4821a6b9
Jim Zike Huang <jim.javathunderbird@gmail.com>
2022-06-23 13:55:35 -0700

    now vue js style v-for is possible

--- research/vue-to-react/parse_vue.js
+++ research/vue-to-react/parse_vue.js
@@ -1,10 +1,18 @@
 const parse = require('node-html-parser').default;
 const render = require("dom-serializer").default;
 const fs = require('fs');
+const { JSDOM } = require("jsdom");
 
-const root = parse(fs.readFileSync('./simple.vue').toString());
+const html = fs.readFileSync('./simple.vue').toString();
 
+const { document } = new JSDOM(html).window;
+
+const root = parse(html);
 root.querySelectorAll('*[v-for]').forEach(node => {
+  console.log(node._attrs['v-for']);
+  console.log(`<!--%for(${node._attrs['v-for']})-->`);
+  node.insertAdjacentHTML('beforebegin',`<%for(${node._attrs['v-for']})%>\n`);
+  node.insertAdjacentHTML('afterend',"\n<%endfor%>");
   node.removeAttribute('v-for');
 });
 
