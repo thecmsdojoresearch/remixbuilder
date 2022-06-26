40af2b126a60ecb8b2867aaf04ca05adbe0cbe35
Jim Zike Huang <jim.javathunderbird@gmail.com>
2022-06-26 15:38:33 -0700

    further structure state update template

--- .remixapp/app/routes/setstate.tsx
+++ .remixapp/app/routes/setstate.tsx
@@ -16,16 +16,20 @@ export default () => {
       }
     },
     incrementCounter(){
-      this.state.counter += 2;
+      this.state.counter += 1;
+    },
+    decrementCounter() {
+      this.state.counter -= 1;
     },
     addNumber() {
       this.state.a.b.c.numbers.push(3);
     },
     onMounted() {
       console.log("mounted...");
-    }
+    },
   }
 
+  ////////////////////// Auto Generated ////////////////
   let [state, setState] = useState(page.state);
 
   page.state = state;
@@ -42,7 +46,8 @@ export default () => {
         <body>
           <h1>{page.state.counter}......</h1>
           <h4>{page.state.a.b.c.numbers}</h4>
-          <button onClick={() => {page.incrementCounter();syncState()}}>Click</button>
+          <button onClick={() => {page.incrementCounter(); syncState()}}>Increment Counter</button>
+          <button onClick={() => {page.decrementCounter(); syncState()}}>Decrement Counter</button>
           <button onClick={() => {page.addNumber(); syncState()}}>Add Number...</button>
           <Scripts />
         </body>
